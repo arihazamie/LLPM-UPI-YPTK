68ed194098cee9e97ed1f5c97b0b6dd67e4c9e34
Ari Hazamie <arihzmii@gmail.com>
2025-08-09 22:00:08 +0700

    FIX: Vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,11 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 
+export const config = {
+  runtime: "edge",
+  regions: ["sin1"],
+};
+
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
